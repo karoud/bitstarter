2b931f0477f3045f078d7f61201d52691b3963e9
Ubuntu <ubuntu@ip-172-31-42-74.us-west-2.compute.internal>
2013-07-16 13:11:08 +0000

    Udate index

--- web.js
+++ web.js
@@ -2,6 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var fs=require('fs');
+var buffer=fs.readFileSync('/home/ubuntu/bitstarter/index.html',function(err,data){
+	if(err){
+		throw err;
+	}
+});
+var str=buffer.toString();
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
